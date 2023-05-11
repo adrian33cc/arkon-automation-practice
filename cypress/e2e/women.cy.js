38d959916554ffa3877ff088625da6a77fd1727b
adrianRD <adrian.33ice@hotmail.com>
2023-05-10 18:16:58 -0600

    Agregando pipeline basico

--- cypress/e2e/women.cy.js
+++ cypress/e2e/women.cy.js
@@ -7,18 +7,18 @@ describe('Agregando un Articulo al carrito,seccion Women', () => {
   
   })
   
-  it('Agregando un articulo con stock', () => {
+  it('CART-03: Agregando un articulo con stock', () => {
 
     cy.get('.top-pagination-content > .compare-form').scrollIntoView()
     cy.get('.last-in-line.first-item-of-tablet-line > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()
     cy.wait(500)
-
+    
     cy.contains('Product successfully added to your shopping cart').should('be.visible')
     cy.contains('There is 1 item in your cart.').should('be.visible')
   })
 
 
-  it('El boton de un articulo SIN stock, debe estar deshabilitado', () => {
+  it('CART-11:El boton de un articulo SIN stock, debe estar deshabilitado', () => {
 
     cy.get('.top-pagination-content > .compare-form').scrollIntoView()
     cy.get(':nth-child(1) > .product-container > .right-block > .button-container > span').should('have.class','disabled')
